619f14cab881161476bc3a2c17b2d250150b046b
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 17:08:46 +0530

    Add files via upload

---
+++ OddEvenindex.java
@@ -0,0 +1,23 @@
+/*
+@Author : Dhanush Kumar M
+Date : 11/08/2022
+Title : Array Program
+Question : Printing odd and even index value of array.
+*/
+class OddEvenindex
+{    
+        static int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};   
+        public static void main(String[] args) 
+		{  
+            System.out.println("odd index value: "); 
+	 	    for (int i = 0; i < arr.length; i = i+2) 
+		        {   
+                   System.out.println(arr[i]);   
+                }
+            System.out.println("even index value: "); 
+	 	    for (int i = 1; i < arr.length; i = i+2) 
+		        {   
+                   System.out.println(arr[i]);   
+                }
+		}
+}	
